54a0aa494f0b158daa9319e71fce91f72aaf9999
xMonUx <107808118+xMonUx@users.noreply.github.com>
2023-03-11 15:15:25 +0100

    Add files via upload

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,66 @@
+// Side bar toggle
 $(document).ready(function() {
 
     function toggleSidebar() {
-      $(".button").toggleClass("active");
-      $("main").toggleClass("move-to-left");
-      $(".sidebar-item").toggleClass("active");
-      $("sidebar").toggleClass("nav-right visible-xs");
+        $(".button").toggleClass("active");
+        $(".sidebar-item").toggleClass("active");
+        $("sidebar").toggleClass("nav-right visible-xs");
+        $(".content").toggleClass("active");
+
     }
   
     $(".button").on("click tap", function() {
-      toggleSidebar();
-    });
-  
-    $(document).keyup(function(e) {
-      if (e.keyCode === 27) {
         toggleSidebar();
-      }
     });
+});
+
+// Settings button
+$(document).ready(function() {
+    $('#btn').click(function() {
+        $('.sidebar').toggleClass('active');
+    });
+
+    $('.settings').click(function() {
+        $('.sidebar-item:not(.settings)').toggleClass('active');
+    });
+
+});
+
+
   
+
+// Loader strony
+window.addEventListener('load',function(){
+    document.querySelector('body').classList.add("loaded")  
   });
+  
 
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  let interval = null;
+  
+  document.getElementById("txt-effect").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 30);
+  };
+  
